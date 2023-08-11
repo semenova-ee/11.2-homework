136c8573b5129e4fee7a5900b3b2a405a8f10bef
Екатерина <ekaterina.s3006@yandex.ru>
2023-08-11 12:43:53 +0300

    modified upper_string

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper_string(somestring) -> str:
+    '''Функция принимает строку
+    возвращает строку заглавными буквами'''
     return somestring.upper()
 
+def title_string(somestring) -> str:
+    '''Функция принимает строку
+    возвращает строку, в которой каждое слово с заглавной буквы'''
+    return somestring.title()
